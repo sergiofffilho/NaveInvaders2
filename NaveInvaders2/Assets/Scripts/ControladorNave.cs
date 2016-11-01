76b381beb137f58bfc3c91870f1b6e20737d9849
sergiofffilho <sergiofffilho@gmail.com>
2016-11-01 17:00:24 -0300

    Rotação da Nave

---
+++ NaveInvaders2/Assets/Scripts/ControladorNave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorNave : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		controladorVelocidade ();
+	}
+
+	void controladorVelocidade(){
+		transform.Rotate(Input.GetAxis("Vertical"),0.0f, -Input.GetAxis("Horizontal"));
+
+	} 
+}
