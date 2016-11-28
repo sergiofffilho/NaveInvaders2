4c265a423061dac1ea1f0919f2cac01562c83a1e
sergiofffilho <sergiofffilho@gmail.com>
2016-11-28 18:23:33 -0300

    Lógica de poder acelerar e frear nave.

--- NaveInvaders2/Assets/Scripts/ControladorNave.cs
+++ NaveInvaders2/Assets/Scripts/ControladorNave.cs
@@ -5,12 +5,15 @@ public class ControladorNave : MonoBehaviour {
 
 	public float velocidade;
 	public float distanciaCamera;
+	float aceleracao;
+	bool acelerando;
 
 	JoyStick joystick;
 
 	void Start () {
 		distanciaCamera = 8;
 		velocidade = 30;
+		aceleracao = 0.1f;
 
 		joystick = GameObject.FindGameObjectWithTag ("Joystick").GetComponent<JoyStick>();
 	}
@@ -19,6 +22,7 @@ public class ControladorNave : MonoBehaviour {
 
 		controladorVelocidade ();
 		controladorCamera ();
+
 	}
 
 	void controladorCamera(){
@@ -32,7 +36,59 @@ public class ControladorNave : MonoBehaviour {
 	void controladorVelocidade(){
 		transform.position += transform.forward * Time.deltaTime * velocidade;
 		transform.Rotate (-joystick.Vertical(),  joystick.Horizontal(),0.0f);
+	}
+
+	public void acelerarBotao(){
+		aceleracao = 0.1f;
+		acelerando = true;
+		InvokeRepeating("acelerar", 0, 0.1f);
+	}
+
+	public void desacelerarBotao(){
+		acelerando = false;
+		InvokeRepeating("desacelerar", 0, 0.1f);
+	}
+
+	void acelerar(){
+		Debug.Log ("a - "+aceleracao);
+		if (aceleracao >= 0 && acelerando) {
+			transform.Translate(Vector3.forward  * aceleracao * Time.deltaTime);
+			aceleracao += 20f * Time.deltaTime;
+
+		} 
+		if (aceleracao <= 0 && !acelerando) {
+			transform.Translate(Vector3.forward  * aceleracao * Time.deltaTime);
+			aceleracao += 20f * Time.deltaTime;
+		}
+		if (aceleracao >= 30 ) {
+			InvokeRepeating("desacelerar", 0, 0.1f);
+			CancelInvoke ("acelerar");
+		}
+		if (aceleracao >= 0 && !acelerando) {
+			aceleracao = 0.1f;
+			CancelInvoke ("acelerar");
+		}
+	}
+
+	void desacelerar(){
+		Debug.Log ("d - "+aceleracao);
+		if (aceleracao <= 32 && acelerando) {
+			transform.Translate(Vector3.forward  * aceleracao * Time.deltaTime);
+			aceleracao -= 20f * Time.deltaTime;
+		}
+		if (aceleracao <= 0 && acelerando) {
+			//aceleracao = 0.1f;
+			CancelInvoke ("desacelerar");
+		}
+		if (aceleracao <= 0.2f && !acelerando) {
+			transform.Translate(Vector3.forward  * aceleracao * Time.deltaTime);
+			aceleracao -= 20f * Time.deltaTime;
+		}
+		if (aceleracao <= -32) {
+			InvokeRepeating("acelerar", 0, 0.1f);
+			CancelInvoke ("desacelerar");
+		}
 
 
-	} 
+	}
 }
